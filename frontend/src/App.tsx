e32b50b9d840a64f9353b12b89c97fa4da3546ce
Francislie <mfrancislie@gmail.com>
2023-08-31 01:13:27 -0700

    Add Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,32 @@
 import { sampleProducts } from "./data"
-
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
 
 function App() {
 
   return (
     <div>
-      <nav>
-        TS-MERN-Ecommerce
-      </nav>
+       <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>tsamazona</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className="nav-link">
+              Cart
+            </a>
+            <a href="/signin" className="nav-link">
+              Sign In
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
       <main>
-        <ul>
+        <Container className='mt-3'>
+          <Row>
           {sampleProducts.map((product) => {
             const {slug, image,name, price} = product
            return (
-              <li key={slug}>
+              <Col key={slug}>
               <img
                 src={image}
                 alt={product.name}
@@ -21,10 +34,11 @@ function App() {
               />
               <h2>{name}</h2>
               <p>${price}</p>
-            </li>
+            </Col>
            )
           })}
-        </ul>
+          </Row>
+        </Container>
       </main>
       <footer>footer</footer>
     </div>
